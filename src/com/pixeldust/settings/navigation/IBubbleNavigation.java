90bd624d73d82481b578b495dff02b3d785f3a1c
sb6596 <shubhamprince111@gmail.com>
2021-11-06 19:10:23 +0100

    PixelDustSettings: Revamp UI for 12.x * This is based on the stripped down Extensions app by AEX (removed aexstats) * This implements Bottom Bubble Navigation using Top Floating Bar with View Pager * Adapt to monet compatibility * Original library can be found here https://github.com/gauravk95/bubble-navigation
    Co-authored-by: gaurav <19gauravk95@gmail.com>
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ src/com/pixeldust/settings/navigation/IBubbleNavigation.java
@@ -0,0 +1,15 @@
+package com.pixeldust.settings.navigation;
+
+import android.graphics.Typeface;
+import com.pixeldust.settings.navigation.BubbleNavigationChangeListener;
+
+@SuppressWarnings("unused")
+public interface IBubbleNavigation {
+    void setNavigationChangeListener(BubbleNavigationChangeListener navigationChangeListener);
+
+    void setTypeface(Typeface typeface);
+
+    int getCurrentActiveItemPosition();
+
+    void setCurrentActiveItem(int position);
+}
